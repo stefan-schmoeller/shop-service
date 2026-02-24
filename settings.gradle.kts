21c82be506526babf4dc2a7b46d3386e8f61f9a6
Stefan Schmoeller <stefan.schmoeller@qaware.de>
2026-02-24 23:11:40 +0100

    Add basic Quarkus setup and dependencies that should be needed

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,13 @@
 pluginManagement {
+    val quarkusPluginVersion: String by settings
+    val quarkusPluginId: String by settings
     repositories {
         mavenCentral()
         gradlePluginPortal()
         mavenLocal()
     }
     plugins {
+        id(quarkusPluginId) version quarkusPluginVersion
     }
 }
 rootProject.name = "shop-service"
