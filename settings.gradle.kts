dcb577ea697cfa66efd793504745dd955c52784f
Stefan Schmoeller <stefan.schmoeller@qaware.de>
2026-02-24 23:07:29 +0100

    Add basic gradle setup

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        mavenLocal()
+    }
+    plugins {
+    }
+}
+rootProject.name = "shop-service"
