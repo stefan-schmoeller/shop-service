f4b673255929863c52178718f76ff369094fcaa5
Benedikt Werner <1benediktwerner@gmail.com>
2026-03-10 13:40:31 +0100

    Use TestTransaction in OrderProviderTest

--- src/test/java/de/qaware/ssh/shop/service/order/OrderProviderTest.java
+++ src/test/java/de/qaware/ssh/shop/service/order/OrderProviderTest.java
@@ -1,5 +1,6 @@
 package de.qaware.ssh.shop.service.order;
 
+import io.quarkus.test.TestTransaction;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ class OrderProviderTest {
     OrderProvider provider;
 
     @Test
+    @TestTransaction
     void combinedTest() {
         UUID orderId = UUID.randomUUID();
         Order orderToStore = new Order(orderId, 1, "receipts/order-" + orderId + ".txt");
